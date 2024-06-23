fa0f1f89e8658ca0644d4cc583625d738e76e372
itamarcasspi <itamarcasspi@gmail.com>
2024-06-23 23:36:56 +0300

    update origin

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.options("*", cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5000",
     methods: ["GET", "POST"],
   },
 });
