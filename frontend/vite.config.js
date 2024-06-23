c7c85ea35f845cf57d52c4d2b83ddf2672e68fc2
itamarcasspi <itamarcasspi@gmail.com>
2024-06-24 01:14:00 +0300

    update vite.config proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port:5000, 
-  //   proxy:{
-  //     "/api":{
-  //       target:"http://localhost:5000"
-  //     }
-  //   }
-  // }
+  server: {
+    port:5000, 
+    proxy:{
+      "/api":{
+        target:process.env.SOCKET_ADD
+      }
+    }
+  }
 })
